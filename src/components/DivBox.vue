ee29cba3ee0d4e0157c09d6baa7dc6b841b12c2c
andfwas <andfwas@gmail.com>
2019-11-15 06:41:47 -0700

    data passed from slider to divs OK

---
+++ src/components/DivBox.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="divbox">
+    <h1>{{ msg }}</h1>
+    <input v-model="multi" type="range" min="1" max="20">
+    <Divs v-bind:mult="parseInt(multi)"></Divs>
+  </div>
+</template>
+
+<script>
+import Divs from './Divs.vue'
+export default {
+  name: 'DivBox',
+  props: {
+    msg: String,
+    mult: Number
+  },
+  components: {
+    Divs
+  },
+  data: function() {
+    return {
+      multi: 10
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Divs.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <button @click="getColor"></button>
+    <div v-for="n in mult" :key="n">
+      <div class="row">
+        <div class="box" v-for="n in mult"  :key="n.toString()">{{n}}</div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Divs',
+  props: {
+    mult: Number
+  },
+  methods: {
+    getColor: function(e) {
+      e.preventDefault()
+      var r = Math.floor(Math.random() * 128)
+      var g = Math.floor(Math.random() * 128)
+      var b = Math.floor(Math.random() * 128)
+      console.log(r)
+      var color = "rgb(" + r + ", " + g + ", " + b + ");"
+      var css = "background-color: " + color
+      console.log(css)
+      return css
+    }
+  }
+};
+</script>
+
+<style scoped>
+  .row {
+    display: flex;
+    flex-flow: row;
+    width: 95%;
+    margin: auto;
+    padding: 0 0 0 0;
+    margin: 0 0 0 0;
+  }
+  .box {
+    width: 4.5vw;
+    outline: none;
+    height: 4.5vw;
+  }
+</style>
